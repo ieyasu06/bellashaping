748fef1f67ae089af2e4000287b4838a6c018e33
john carlo bernabe <johncarlobernabe@yahoo.com.pj>
2021-11-16 02:47:38 +0800

    init
    inittial push

---
+++ index.php
@@ -0,0 +1,363 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Bella Shaping -SHAPING THE FUTURE OF BEAUTY</title>
+  <meta content="" name="description">
+
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <!-- <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon"> -->
+
+  <link rel="apple-touch-icon" sizes="180x180" href="assets/img/favicon/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="assets/img/favicon/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="assets/img/favicon/favicon-16x16.png">
+  <link rel="manifest" href="assets/img/favicon/site.webmanifest">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: FlexStart - v1.7.0
+  * Template URL: https://bootstrapmade.com/flexstart-bootstrap-startup-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="header fixed-top">
+    <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
+
+      <a href="index.html" class="logo d-flex align-items-center">
+        <!-- <img src="assets/img/logo.png" alt=""> -->
+        <span>Bella Shaping</span>
+      </a>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+          <li><a class="nav-link scrollto" href="#services">Services</a></li>
+          <li><a class="nav-link scrollto" href="#about">About</a></li>
+          <li><a class="nav-link scrollto" href="#testimonials">Testimonials</a></li>
+          <li><a class="nav-link scrollto" href="#contact">Contact Us</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="hero d-flex align-items-center">
+    <video id="video1" preload="" autoplay="" muted="" playsinline="" loop="" style="width:100%; position: absolute; transform: rotateY(180deg); -webkit-transform:rotateY(180deg); /* Safari and Chrome */ -moz-transform:rotateY(180deg); /*height: 100vh;*/ padding-top: 60px; object-fit: fill;">
+      <source src="assets/img/banner/Banner Video.mp4" type="video/mp4">
+    </video>
+
+    <div class="container">
+      <div class="row">
+
+        <div class="col-lg-6 hero-img" data-aos="zoom-out" data-aos-delay="200">
+
+        </div>
+
+        <div class="col-lg-6 text-end"> <!-- d-flex flex-column justify-content-center -->
+          
+          <img src="assets/img/logo-bella.png" style="max-width: 58%; position: relative;" class="rounded float-right" alt="">
+          <h1 data-aos="fade-up">SHAPING THE</h1>
+          <h1 data-aos="fade-up" data-aos-delay="400">FUTURE OF BEAUTY</h1>
+          <div data-aos="fade-up" data-aos-delay="600">
+            <div class="text-center text-lg-end">
+              <a href="#about" class="btn-get-started scrollto d-inline-flex align-items-center justify-content-center align-self-center">
+                <span>Book Now</span>
+                <!-- <i class="bi bi-arrow-right"></i> -->
+              </a>
+            </div>
+          </div>
+        </div>
+        
+      </div>
+    </div>
+
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+    <!-- ======= Services Section ======= -->
+    <section id="services" class="services">
+
+      <div class="container" data-aos="fade-up">
+
+        <header class="section-header">
+          <h2>Services</h2>
+          <!-- <p>Services</p> -->
+        </header>
+
+        <div class="row gy-4">
+
+          <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="200">
+            <div class="service-box blue">
+              <i class="ri-discuss-line icon"></i>
+              <h3>Nesciunt Mete</h3>
+              <p>Provident nihil minus qui consequatur non omnis maiores. Eos accusantium minus dolores iure perferendis tempore et consequatur.</p>
+              <!-- <a href="#" class="read-more"><span>Read More</span> <i class="bi bi-arrow-right"></i></a> -->
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="300">
+            <div class="service-box orange">
+              <i class="ri-discuss-line icon"></i>
+              <h3>Eosle Commodi</h3>
+              <p>Ut autem aut autem non a. Sint sint sit facilis nam iusto sint. Libero corrupti neque eum hic non ut nesciunt dolorem.</p>
+              <!-- <a href="#" class="read-more"><span>Read More</span> <i class="bi bi-arrow-right"></i></a> -->
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="400">
+            <div class="service-box green">
+              <i class="ri-discuss-line icon"></i>
+              <h3>Ledo Markt</h3>
+              <p>Ut excepturi voluptatem nisi sed. Quidem fuga consequatur. Minus ea aut. Vel qui id voluptas adipisci eos earum corrupti.</p>
+              <!-- <a href="#" class="read-more"><span>Read More</span> <i class="bi bi-arrow-right"></i></a> -->
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="500">
+            <div class="service-box red">
+              <i class="ri-discuss-line icon"></i>
+              <h3>Asperiores Commodi</h3>
+              <p>Non et temporibus minus omnis sed dolor esse consequatur. Cupiditate sed error ea fuga sit provident adipisci neque.</p>
+              <!-- <a href="#" class="read-more"><span>Read More</span> <i class="bi bi-arrow-right"></i></a> -->
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="600">
+            <div class="service-box purple">
+              <i class="ri-discuss-line icon"></i>
+              <h3>Velit Doloremque.</h3>
+              <p>Cumque et suscipit saepe. Est maiores autem enim facilis ut aut ipsam corporis aut. Sed animi at autem alias eius labore.</p>
+              <!-- <a href="#" class="read-more"><span>Read More</span> <i class="bi bi-arrow-right"></i></a> -->
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="700">
+            <div class="service-box pink">
+              <i class="ri-discuss-line icon"></i>
+              <h3>Dolori Architecto</h3>
+              <p>Hic molestias ea quibusdam eos. Fugiat enim doloremque aut neque non et debitis iure. Corrupti recusandae ducimus enim.</p>
+              <!-- <a href="#" class="read-more"><span>Read More</span> <i class="bi bi-arrow-right"></i></a> -->
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+
+    </section><!-- End Services Section -->
+
+    <!-- ======= About Section ======= -->
+    <section id="about" class="about">
+
+      <div class="container-fluid gx-0" data-aos="fade-up">
+        <div class="row gx-0">
+
+          <div class="col-lg-5 d-flex align-items-center" data-aos="zoom-out" data-aos-delay="200">
+            <img src="assets/img/About Us.jpg" class="img-fluid" alt="">
+          </div>
+
+          <div class="col-lg-7 d-flex flex-column justify-content-center" data-aos="fade-up" data-aos-delay="200">
+            <div class="content">
+
+              <h2>ABOUT US</h2>
+              <p>Our goal is to motivate and inspire everyone to feel beautiful while working towards the best version of themselves. Our passion is to provide the right services to help our clients look and feel beautiful.</p>
+              <p>It is our vision to inspire beauty and self care to everyone.</p>
+              <div class="text-center text-lg-end">
+                <a href="#" class="btn-read-more d-inline-flex align-items-center justify-content-center align-self-center">
+                  <span>Learn More</span>
+                  <!-- <i class="bi bi-arrow-right"></i> -->
+                </a>
+              </div>
+            </div>
+          </div>
+
+          
+
+        </div>
+      </div>
+
+    </section><!-- End About Section -->
+
+    <!-- ======= Testimonials Section ======= -->
+    <section id="testimonials" class="testimonials">
+
+      <div class="container" data-aos="fade-up">
+
+        <header class="section-header">
+          <h2>Our Review</h2>
+          <!-- <p>Our Review</p> -->
+        </header>
+
+        <div class="testimonials-slider swiper" data-aos="fade-up" data-aos-delay="200">
+          <div class="swiper-wrapper">
+
+            <div class="swiper-slide">
+              <div class="testimonial-item">
+                <div class="profile mt-auto">
+                  <img src="assets/img/testimonials/testimonials-1.jpg" class="testimonial-img" alt="">
+                </div>
+                <p>
+                  The results areamazing! I am so happy with how my skin looks like now.
+                  <h3>MARIA LUX</h3>
+                </p>
+              </div>
+            </div><!-- End testimonial item -->
+
+            <div class="swiper-slide">
+              <div class="testimonial-item">
+                <div class="profile mt-auto">
+                  <img src="assets/img/testimonials/testimonials-2.jpg" class="testimonial-img" alt="">
+                </div>
+                <p>
+                  Excellent and quick service, probably the best I have ever had.
+                  <h3>CHRISTINE</h3>
+                </p>
+              </div>
+            </div><!-- End testimonial item -->
+
+            <div class="swiper-slide">
+              <div class="testimonial-item">
+                <div class="profile mt-auto">
+                  <img src="assets/img/testimonials/testimonials-3.jpg" class="testimonial-img" alt="">  
+                </div>
+                <p>
+                  I love the wood therapy. It's so relaxing. Rhoda did a good job! I'll be back after 2 weeks.
+                  <h3>JESSICA</h3>
+                </p>
+              </div>
+            </div><!-- End testimonial item -->
+
+          </div>
+          <div class="swiper-pagination"></div>
+        </div>
+
+      </div>
+
+    </section><!-- End Testimonials Section -->
+
+
+    <section id="instagram" class="instagram">
+
+      <div class="container" data-aos="fade-up">
+
+        <header class="section-header">
+          <h2>BELLA SHAPING ON INSTAGRAM</h2>
+        </header>
+        <div  id="instafeed" class="row gy-4" data-aos="fade-up" data-aos-delay="200"></div>
+        <!-- <div id="instafeed"></div> -->
+      </div>
+    </section>
+
+
+    <!-- ======= About Section ======= -->
+    <section id="contact" class="about">
+
+      <div class="container gx-0" data-aos="fade-up">
+        <div class="row gx-0">
+
+          <div class="col-lg-5 d-flex align-items-center" data-aos="zoom-out" data-aos-delay="200">
+            <h2>CONTACT US</h2>
+          </div>
+
+          <div class="col-lg-7 d-flex flex-column justify-content-center" data-aos="fade-up" data-aos-delay="200">
+            <div class="content">
+              <p><i class="bi bi-geo-alt me-2" style="color: #FF66C4;"></i> 5541 Arrow Highway, Montclair, CA 91763</p>
+              <p><i class="bi bi-telephone me-2" style="color: #FF66C4;"></i> 909-990-7644</p>
+              <p><i class="bi bi-envelope me-2" style="color: #FF66C4;"></i> <a href="mailto:bellashaping@gmail.com">bellashaping@gmail.com</a></p>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+    </section><!-- End About Section -->
+
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer" class="footer">
+
+    <div class="container" style="visibility: hidden;">
+      <div class="copyright">
+        &copy; Copyright <strong><span>FlexStart</span></strong>. All Rights Reserved
+      </div>
+      <div class="credits">
+        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/purecounter/purecounter.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+  <script type="text/javascript" src="assets/js/instafeed/dist/instafeed.min.js"></script>
+
+  <?php
+  $now = strtotime(date("Y-m-d")); // or your date as well
+  $your_date = strtotime(date("2022-01-15"));
+  $datediff = $now - $your_date;
+
+  $datediff2 = round($datediff / (60 * 60 * 24));
+
+  if($datediff2%60==0)
+  {
+    echo 1;
+  }
+  else
+    echo 2;
+
+  ?>
+
+  <script type="text/javascript">
+    var feed = new Instafeed({
+      accessToken: 'IGQVJXY1lCTkl1VnNPN0FYWnJNWnRjTG1UUWYzMThjQlUwazludm81RGlPeW4wdnZAkWkx6RnBYaUVlZAXBrcTFmVVZA3RDJtX1RPaUVIQTZAraWdHYmlJZAk9sdzZAMQWJmdElDTWY5a1JB',
+      template: '<div class="col-lg-4 col-md-6 portfolio-item filter-app"><a href="{{link}}" title="{{caption}}" target="_blank"><img src="{{image}}" class="img-fluid" title="{{caption}}"></a></div>'
+    });
+    feed.run();
+</script>
+
+</body>
+
+</html>
+
+
+          
